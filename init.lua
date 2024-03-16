d6dd185f47f15bc1809d308ddcec660f549bdd09
tolios <apostolos.kakampakos@gmail.com>
2024-03-16 03:16:33 +0200

    changed to github theme

--- init.lua
+++ init.lua
@@ -719,25 +719,18 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
-    priority = 1000, -- make sure to load this before all the other start plugins
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 2000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {
+        options = {
+          transparent = true,
+        },
+      }
 
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
-      -- custom line colorings...
-      vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white' })
-      vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'white' })
-      vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white' })
+      vim.cmd 'colorscheme github_dark_high_contrast'
     end,
   },
 
